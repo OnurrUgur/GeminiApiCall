2f581ee370e524ecc266b11e18fb39c464fafc09
OnurrUgur <onurugur.prf@gmail.com>
2024-03-07 19:01:35 +0300

    Initial Commit

---
+++ GeminiApiCall/GeminiApiCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiApiCallApp.swift
+//  GeminiApiCall
+//
+//  Created by Onur Uğur on 7.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct GeminiApiCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
